e1043b425a60afff5b4ce955f60d952a9dee12cf
Mike Francis <mike.francis@live.com>
2023-03-21 16:34:57 -0700

    Update packaging batch file

--- packageSolution.cmd
+++ packageSolution.cmd
@@ -1,17 +1,21 @@
-rmdir /s /q ResponsiveIFrameControlSolution
-md ResponsiveIFrameControlSolution
-cd ResponsiveIFrameControlSolution
-cmd /c pac solution init --publisher-name mwappconsult --publisher-prefix wmappcon
-cmd /c pac solution add-reference --path ..\responsiveIFrameControl 
+REM Update publisherName and publisherPrefix below for your environment
+set publisherName=<yourpubname>
+set publisherPrefix=<yourpubprefix>
+set solutionName=ResponsiveIFrameControlSolution
+set controlName=responsiveIFrameControl
+
+rmdir /s /q %solutionName%
+md %solutionName%
+cd %solutionName%
+cmd /c pac solution init --publisher-name %publisherName% --publisher-prefix %publisherPrefix%
+cmd /c pac solution add-reference --path ..\%controlName% 
 msbuild /t:build /restore
 if "%1"=="push" goto push
 goto end
 :push
 REM Note: Increment version in manifest prior to push to update existing deployment
-cd ..\responsiveIFrameControl
+cd ..\%controlName%
 cmd /c pac pcf version --strategy manifest
-cmd /c pac pcf push --publisher-prefix wmappcon
+cmd /c pac pcf push --publisher-prefix %publisherPrefix%
 :end
 cd ..
-
-
